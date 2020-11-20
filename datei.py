9d91913603abafc399e27adea2d6743370c2342a
TimOtto <tim.otto@live.de>
2020-11-20 15:11:28 +0100

    bisschen mit numpy und sklearn rumprobiert

--- datei.py
+++ datei.py
@@ -1,11 +1,26 @@
-# kommentar
-# zweiter kommentar
-print("hallo ich bins")
-print(1+1)
-# moin leute trymacs hier
-print(3)
-i = 0
-while (i < 3):
-    print(i)
-    i = i +1
-    print("ich baue lego roboter")
+import numpy as np
+from sklearn import linear_model
+
+a = np.zeros(3) #array mit 3 nullen
+print(a.shape)
+
+b = np.array([1,5,6]) #array
+print(b[0])
+
+c_list = [3,5,7,9] #liste
+c_array = np.array(c_list) #array bekommt liste uebergeben
+print(c_array[2])
+
+e_list = [[1,2,3], [4,5,6]] #zwei dimensionale liste
+e_array = np.array(e_list) #zweidimensionales array
+print(e_array[1][0])
+
+print(e_array < 3) #testet welche indize kleiner als 3 sind
+print(e_array[e_array<3]) # gibt genau die indize aus bei denen die bedingung zutrifft
+
+#scikitlearn ausprobiert bissl
+reg = linear_model.LinearRegression()
+reg.fit([[0,0], [1,1], [2,2]], [0,1,2])
+LinearRegression()
+reg.coef_
+array([0.5, 0.5])
